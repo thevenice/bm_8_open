2becf5bccf0ef39f62b988c5202a095494a80b15
thevenice <pp886655@gmail.com>
2020-01-27 00:12:21 +0530

    open

---
+++ precache-manifest.6b100e8346c46f942fbde41c5180b238.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ec8def46c586870ab9bf7d3a3e3807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506ddd397e8af8b0f771",
+    "url": "/static/css/main.ea3b1a5a.chunk.css"
+  },
+  {
+    "revision": "6aa865dde75dd9430fcf",
+    "url": "/static/js/2.806b848c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.806b848c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "506ddd397e8af8b0f771",
+    "url": "/static/js/main.2c99c9a3.chunk.js"
+  },
+  {
+    "revision": "10bee7c4297498bff5b2",
+    "url": "/static/js/runtime-main.4784d7de.js"
+  },
+  {
+    "revision": "b9a8f90e7bf16b9297ede5146a69f530",
+    "url": "/static/media/SocialBottom.b9a8f90e.png"
+  },
+  {
+    "revision": "b24bb1d006f5870ff6732bd8edffc65d",
+    "url": "/static/media/branding_man.b24bb1d0.jpg"
+  },
+  {
+    "revision": "9389da729cd6a1810ef65c5981cca3c6",
+    "url": "/static/media/choices-colour.9389da72.svg"
+  },
+  {
+    "revision": "2355471805b66222839d57adc396bb4c",
+    "url": "/static/media/event.23554718.png"
+  },
+  {
+    "revision": "a711c867314978e9200a9560646e94a9",
+    "url": "/static/media/handshake-colour.a711c867.svg"
+  },
+  {
+    "revision": "bcba4c09469442aaaeef3086911e90d5",
+    "url": "/static/media/onad.bcba4c09.png"
+  }
+]);
